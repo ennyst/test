cc720616de728646586d5726aac8dd87607a8823
ennys <37688275+ennyst@users.noreply.github.com>
2018-03-23 16:32:33 +0700

    halodof

--- prime.py
+++ prime.py
@@ -3,10 +3,10 @@ def prime (bilangan):
     for i in range(1, bilangan+1):
         if (bilangan % i) == 0: # modulus i karena habis dibagi dirinya dan 3
             count +=1
-    if (count==2):
-        return ("prime")
-    else:
-        return ("false")
+    # if (count==2):
+    #     return ("prime")
+    # else:
+    #     return ("false")
 
 tes = prime(8)
 print (tes)
