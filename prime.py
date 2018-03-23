a690001693d3e0b4df0f48660bb9d1b27a3a491f
Your Name <macbook@Macbooks-MacBook-Pro-2.local>
2018-03-23 15:41:01 +0700

    testing commit

---
+++ prime.py
@@ -0,0 +1,18 @@
+def prime (bilangan):
+    count =0
+    for i in range(1, bilangan+1):
+        if (bilangan % i) == 0: # modulus i karena habis dibagi dirinya dan 3
+            count +=1
+    if (count==2):
+        return ("prime")
+    else:
+        return ("false")
+
+tes = prime(8)
+print (tes)
+
+
+
+
+
+
